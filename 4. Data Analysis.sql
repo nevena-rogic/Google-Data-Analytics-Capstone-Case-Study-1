9b566260a5ed1e3ee2ca115c185dadeb1164b70b
nevena-rogic <nevenarogic7@gmail.com>
2025-02-17 18:44:28 +0100

    Update 4. Data Analysis.sql

--- 4. Data Analysis.sql
+++ 4. Data Analysis.sql
@@ -12,3 +12,14 @@ GROUP BY
 ORDER BY 
     member_casual,          -- Order by rider type
     total_trips             -- Order by total trips
+
+--distribution of users per month    
+SELECT 
+    month,                  -- Month of the trip
+    member_casual,          -- Type of rider (member or casual)
+    COUNT(ride_id) AS total_trips -- Total number of trips
+FROM 
+    [2024_tripdata.cleaned_combined_data] -- Source table
+GROUP BY 
+    month,                  -- Group by month
+    member_casual           -- Group by rider type
