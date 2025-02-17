b5f7eae089f4a37afc71967d10df14f626502b8a
nevena-rogic <nevenarogic7@gmail.com>
2025-02-17 18:02:23 +0100

    Update 4. Data Analysis.sql

--- 4. Data Analysis.sql
+++ 4. Data Analysis.sql
@@ -1,1 +1,14 @@
 --Data Analysis
+--bikes types used by riders
+SELECT TOP 100
+    member_casual,          -- Type of rider (member or casual)
+    rideable_type,          -- Type of bike used
+    COUNT(*) AS total_trips -- Total number of trips
+FROM 
+    [2024_tripdata.cleaned_combined_data] -- Source table
+GROUP BY 
+    member_casual,          -- Group by rider type
+    rideable_type           -- Group by bike type
+ORDER BY 
+    member_casual,          -- Order by rider type
+    total_trips             -- Order by total trips
